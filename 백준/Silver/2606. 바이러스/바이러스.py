01f7199675af34316a9e2f4905302b536fdd48cd
haninan94 <109258052+haninan94@users.noreply.github.com>
2022-08-25 11:27:13 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,26 @@
-computer = int(input())
-networks = int(input())
-visited = [False] * (computer+1)
-graph = [[] for _ in range(computer+1)]
+def bfs(v):
+    queue = [v]
+    visited[v] = True
+    result = 0
 
-for i in range(networks):
+    while queue: # queue 가 비어있을 때 까지
+        new_v = queue.pop(0)
+        for i in graph[new_v]:
+            if not visited[i]: # i가 방문처리 안되어있다면
+                visited[i] = True
+                queue.append(i)
+                result += 1
+
+    return result
+
+N = int(input()) # 컴퓨터의 수
+network = int(input()) # 네트워크 연결 수
+visited = [False] * (N + 1) # 방문처리
+graph = [[] for _ in range(N+1)]
+
+for i in range(network):
     v1, v2 = map(int, input().split())
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-def dfs(n):
-    visited[n] = True # 방문처리
-
-    for new_visit in graph[n]:
-        if visited[new_visit] == False:
-            dfs(new_visit)
-dfs(1)
-sum = 0
-for i in range(len(visited)):
-    if visited[i] == True:
-        sum += 1
-print(sum-1)
+print(bfs(1))
